1abec17bf3ba1a3a34c1bc9d230b4234afd0c696
Manoel <125422009+ManoelMorais@users.noreply.github.com>
2024-02-10 23:00:40 -0300

    header 0.0.2

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 
+import { SlNotebook } from "react-icons/sl";
+
 import logo from "../../assets/logo-nlw-expert.svg";
 
 const Header = () => {
   return (
-    <div className="w-full bg-slate-700 text-slate-400 p-6 flex justify-around shadow-3xl">
+    <div className="w-full bg-slate-700 text-slate-900 p-6 flex justify-around shadow-3xl">
       <div>
         <img src={logo} alt="logo" />
       </div>
-      <div className="flex items-center space-x-5">
-        <h1 className="font-semibold">Bloco de Notas</h1>
+      <div className="flex items-center gap-3">
+        <h1 className="font-semibold text-2xl">BLOCO DE NOTAS</h1>
+        <SlNotebook className="text-2xl"/>
       </div>
     </div>
   );
